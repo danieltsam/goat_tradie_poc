c1c9698f707e9e3e628fec75cd024f86384dbffb
Lachguin <ltkeates@outlook.com>
2026-05-23 16:54:50 +1000

    feat: Added pages folder and home page
    Home page for eventual schedule creation

---
+++ abws_poc/lib/pages/schedulehome.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ScheduleHomePage extends StatelessWidget {
+  const ScheduleHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
