0b441320f3a6d31da4018c842ae3b9cdb7ac81cd
Lachguin <ltkeates@outlook.com>
2026-05-23 22:11:49 +1000

    feat: Added appBar, fonts and icons
    Imported directly from GOAT Tradie files

--- abws_poc/lib/pages/schedulehome.dart
+++ abws_poc/lib/pages/schedulehome.dart
@@ -5,6 +5,26 @@ class ScheduleHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('A Better Weekly Structure',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold
+        )
+        ),
+        backgroundColor: Colors.red,
+        elevation: 0.0,
+        centerTitle: true,
+        leading: Container(
+          margin: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Color(0xffF7F8F8),
+            borderRadius: BorderRadius.circular(10)
+          ),
+        )
+      )
+    );
   }
 }
