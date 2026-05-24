5dd166d27751990ec36501238521f862d381f965
Max-P <122199194+max-padovan@users.noreply.github.com>
2026-05-24 16:23:31 +1000

    Cleaned code a bit

--- abws_poc/lib/main.dart
+++ abws_poc/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'HighVoltage'),
-      home: const ScheduleHomePage()
+      home: const ScheduleHomePage(),
     );
   }
 }
--- abws_poc/lib/pages/schedulehome.dart
+++ abws_poc/lib/pages/schedulehome.dart
@@ -1,105 +1,88 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
-final List<String> weekdays = [
-    'Mon',
-    'Tue',
-    'Wed',
-    'Thu',
-    'Fri',
-    'Sat',
-    'Sun'
-  ];
-  
-
+final List<String> weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
 class ScheduleHomePage extends StatelessWidget {
   const ScheduleHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-      
-    final availableWidth = MediaQuery.of(context).size.width - 60;
     return Scaffold(
       appBar: appBar(),
       floatingActionButton: Row(
-  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-  children: [
-    FloatingActionButton(
-      heroTag: "btn1",
-      onPressed: () {},
-      child: Icon(Icons.help),
-    ),
-    SizedBox(width: 10),
-    FloatingActionButton(
-      heroTag: "btn2",
-      onPressed: () {},
-      child: Icon(Icons.add),
-    ),
-    SizedBox(width: 10),
-    FloatingActionButton(
-      heroTag: "btn3",
-      onPressed: () {},
-      child: Icon(Icons.arrow_right)
-    )
-  ],
-),
-      bottomNavigationBar: BottomAppBar(
-          color: Colors.transparent,
-          child: Container(height: 10)
-        ),
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          FloatingActionButton(
+            heroTag: "btn1",
+            onPressed: () {},
+            child: const Icon(Icons.help),
+          ),
+
+          const SizedBox(width: 10),
+          FloatingActionButton(
+            heroTag: "btn2",
+            onPressed: () {},
+            child: const Icon(Icons.add),
+          ),
+
+          const SizedBox(width: 10),
+          FloatingActionButton(
+            heroTag: "btn3",
+            onPressed: () {},
+            child: const Icon(Icons.arrow_right),
+          ),
+        ],
+      ),
+
+      bottomNavigationBar: const BottomAppBar(
+        color: Colors.transparent,
+        child: SizedBox(height: 10),
+      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      );
+    );
   }
 
   AppBar appBar() {
     return AppBar(
-      title: Text('A Better Weekly Structure',
-      style: TextStyle(
-        color: Colors.black,
-        fontSize: 18,
-      )
+      title: const Text(
+        'A Better Weekly Structure',
+        style: TextStyle(color: Colors.black, fontSize: 18),
       ),
       backgroundColor: Colors.red,
       elevation: 0.0,
       centerTitle: true,
       leading: GestureDetector(
-        onTap: () {
-        },
-        child: Container (
-        margin: EdgeInsets.all(10),
-        alignment: Alignment.center,
-        child: Image.asset('assets/icons/back_arrow.png',
-        height: 20,
-        width: 20
+        onTap: () {},
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Image.asset(
+            'assets/icons/back_arrow.png',
+            height: 20,
+            width: 20,
+          ),
         ),
-        decoration: BoxDecoration(
-          color: Colors.red,
-          borderRadius: BorderRadius.circular(10)
-        ),  
-      ),
       ),
       actions: [
         Container(
-        margin: EdgeInsets.all(10),
-        alignment: Alignment.center,
-        width: 37,
-        child: Image.asset('assets/icons/goat_temp.png',
-        height: 20,
-        width: 20
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          width: 37,
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Image.asset(
+            'assets/icons/goat_temp.png',
+            height: 20,
+            width: 20,
+          ),
         ),
-        decoration: BoxDecoration(
-          color: Colors.red,
-          borderRadius: BorderRadius.circular(10)
-        ),  
-      )
       ],
-      
     );
   }
 }
-
-
-
-
-
