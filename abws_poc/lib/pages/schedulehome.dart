a6a5d820dd93a209a111f5c37379c54a6c300d6b
Max-P <122199194+max-padovan@users.noreply.github.com>
2026-05-24 16:13:36 +1000

    more learning

--- abws_poc/lib/pages/schedulehome.dart
+++ abws_poc/lib/pages/schedulehome.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ScheduleHomePage extends StatelessWidget {
   const ScheduleHomePage({super.key});
@@ -6,25 +7,48 @@ class ScheduleHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('A Better Weekly Structure',
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 18,
-          fontWeight: FontWeight.normal
-        )
-        ),
-        backgroundColor: Colors.red,
-        elevation: 0.0,
-        centerTitle: true,
-        leading: Container(
-          margin: EdgeInsets.all(10),
-          decoration: BoxDecoration(
-            color: Color(0xffF7F8F8),
-            borderRadius: BorderRadius.circular(10)
-          ),
-        )
-      )
+      appBar: appBar()
     );
   }
+
+
+
+
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text('A Better Weekly Structure',
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 18,
+        fontWeight: FontWeight.normal
+      )
+      ),
+      backgroundColor: Colors.red,
+      elevation: 0.0,
+      centerTitle: true,
+
+      leading: GestureDetector(
+        onTap: () {
+          // Add your action here, for example navigating back:
+          // Navigator.pop(context);
+        },
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(10)
+          ),
+          child: SvgPicture.asset(
+            'assets/icons/left-arrow-svgrepo-com.svg',
+            height: 25,
+            width: 25,
+          ),
+        ),
+      ),
+    );
+  }
+
+
 }
