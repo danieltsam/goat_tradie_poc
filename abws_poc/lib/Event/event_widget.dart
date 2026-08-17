189c1a57443b5e3bb390f5158f7eeb449523d39b
Lachguin <ltkeates@outlook.com>
2026-08-17 12:18:43 +1000

    feat: Added timepicker to starttime and end time
    Currently no functionality

--- abws_poc/lib/Event/event_widget.dart
+++ abws_poc/lib/Event/event_widget.dart
@@ -6,7 +6,6 @@ class EventWidget extends StatelessWidget {
     super.key,
     required this.model,
     required this.heightPerHour,
-  });
 
   final EventModel model; //The data model for this widget
   final double heightPerHour; //The calculated height this widget will be so it is proportional to the height of the day.
@@ -17,6 +16,7 @@ class EventWidget extends StatelessWidget {
     final start = timeToDouble(model.startTime);
     final end = timeToDouble(model.endTime);
     final duration = end - start;
+    
 
     return Positioned(
       top: start * heightPerHour,
@@ -43,15 +43,80 @@ class EventWidget extends StatelessWidget {
                     ),
                     const SizedBox(height: 8),
 
-                    Text(
-                      'Start Time: ${model.startTime.format(context)}', // Want to make this a time picker, so can change time
-                    ),
-                    const SizedBox(height: 8),
+                    //Start Time
+                    InkWell(
+                      onTap: () async {
+                        final TimeOfDay? newStartTime =
+                            await showTimePicker(
+                          context: context,
+                          initialTime: model.startTime,
+                        );
+                        // Currently doesn't let you edit the time, using SetState() I think it will automatically update the duration / height
+                      },
 
-                    Text(
-                      'End Time: ${model.endTime.format(context)}', // Want to make this a time picker, so can change time
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 8,
+                        ),
+
+                        child: Row(
+                          children: [
+                            const Text(
+                              'Start Time: ',
+                              style: TextStyle(fontWeight: FontWeight.bold,
+                              ),
+                            ),
+
+                            Text(
+                              model.startTime.format(context),
+                              style: const TextStyle(decoration: TextDecoration.underline,
+                              ),
+                            ),
+
+                            const SizedBox(width: 8),
+                            const Icon(Icons.access_time, size: 18,
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                    const SizedBox(height: 8),
+
+                    // End Time
+                     InkWell(
+                      onTap: () async {
+                        final TimeOfDay? newEndTime =
+                            await showTimePicker(
+                          context: context,
+                          initialTime: model.endTime,
+                        );
+                      // Currently doesn't let you edit the time, using SetState() I think it will automatically update the duration / height
+                      },
+
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric( vertical: 8,
+                        ),
+
+                        child: Row(
+                          children: [
+                            const Text(
+                              'End Time: ',
+                              style: TextStyle( fontWeight: FontWeight.bold,
+                              ),
+                            ),
+
+                            Text(
+                              model.endTime.format(context),
+                              style: const TextStyle(decoration: TextDecoration.underline,
+                              ),
+                            ),
+
+                            const SizedBox(width: 8),
+                            const Icon(Icons.access_time, size: 18,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
 
                     Text(
                       'Duration: ${duration.toStringAsFixed(1)} hours',
