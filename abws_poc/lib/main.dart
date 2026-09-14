74f4004a23398a728a25c4a77a107adc9acaafc8
Lachguin <ltkeates@outlook.com>
2026-09-14 11:51:35 +1000

    feat: Tablet view, low functionality
    Lot of non working buttons, legend non working but it's chill

--- abws_poc/lib/main.dart
+++ abws_poc/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:abws_poc/Schedule/schedule_widget.dart';
+import 'package:abws_poc/Schedule/scheduleTabhome.dart';
 
 import 'package:flutter/material.dart';
 
@@ -15,7 +16,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Inter'),
-      home: const ScheduleHomePage(),
+      home: const ScheduleTabHomePage(), // For testing, I chnaged this to launch the Tablet view, 
+      // For mobile view, const ScheduleHomePage() should be used instead.
     );
   }
 } 
